d6e3bcc3742790cacb9dea0aeb0ad9470323c1aa
Václav Pelíšek <peldax@gmail.com>
2020-03-23 03:06:10 +0100

    Create Argument.php

---
+++ src/Argument/Argument.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PGQL\Argument;
+
+final class Argument
+{
+    use \Nette\SmartObject;
+
+    private string $name;
+    private \PGQL\Type\Inputable $type;
+    private ?\PGQL\Value\InputValue $defaultValue;
+
+    public function __construct(string $name, \PGQL\Type\Inputable $type, $defaultValue = null)
+    {
+        $this->name = $name;
+        $this->type = $type;
+
+        if (\func_num_args() === 3) {
+            $defaultValue = new \PGQL\Value\InputValue($type, $defaultValue);
+        }
+
+        $this->defaultValue = $defaultValue;
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function getType() : \PGQL\Type\Inputable
+    {
+        return $this->type;
+    }
+
+    public function getDefaultValue() : ?\PGQL\Value\InputValue
+    {
+        return $this->defaultValue;
+    }
+}
