8e00fc53e210f476be1f5079aff9ffcef8176377
Václav Pelíšek <peldax@gmail.com>
2020-03-26 22:32:01 +0100

    Create GivenValueTest.php

---
+++ tests/GivenValueTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Value;
+
+final class GivenValueTest extends \PHPUnit\Framework\TestCase
+{
+    public function simpleDataProvider() : array
+    {
+        return [
+            [123, 'name'],
+            [123.123, 'name'],
+            ['123', 'name'],
+            [true, 'name'],
+            [[], 'name'],
+            [[123, true], 'name'],
+            [new \PGQL\Value\Value('inner'), 'name'],
+            [[new \PGQL\Value\Value('inner')], 'name'],
+        ];
+    }
+
+    /**
+     * @dataProvider simpleDataProvider
+     */
+    public function testSimple($rawValue, string $name): void
+    {
+        $value = new \PGQL\Value\GivenValue($rawValue, $name);
+
+        self::assertSame($name, $value->getName());
+        self::assertSame($rawValue, $value->getValue());
+    }
+}
