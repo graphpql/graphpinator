8673aad41ff2c830c9b82e0105cbe92ffccc5bc3
Václav Pelíšek <peldax@gmail.com>
2020-03-31 17:37:19 +0200

    TokenContainer

---
+++ src/Graphpinator/Tokenizer/TokenContainer.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Infinityloop\Graphpinator\Tokenizer;
+
+final class TokenContainer implements \IteratorAggregate
+{
+    use \Nette\SmartObject;
+
+    private array $tokens = [];
+    private int $currentIndex = 0;
+
+    public function __construct(string $source, bool $skipNotRelevant = true)
+    {
+        $tokenizer = new \Infinityloop\Graphpinator\Tokenizer\Tokenizer($source, $skipNotRelevant);
+
+        foreach ($tokenizer as $token) {
+            $this->tokens[] = $token;
+        }
+    }
+
+    public function hasPrev() : bool
+    {
+        return \array_key_exists($this->currentIndex - 1, $this->tokens);
+    }
+
+    public function hasNext() : bool
+    {
+        return \array_key_exists($this->currentIndex + 1, $this->tokens);
+    }
+
+    public function getCurrent() : Token
+    {
+        return $this->tokens[$this->currentIndex];
+    }
+
+    public function getPrev() : Token
+    {
+        if (!$this->hasPrev()) {
+            throw new \Exception('Unexpected end.');
+        }
+
+        return $this->tokens[--$this->currentIndex];
+    }
+
+    public function getNext() : Token
+    {
+        if (!$this->hasNext()) {
+            throw new \Exception('Unexpected end.');
+        }
+
+        return $this->tokens[++$this->currentIndex];
+    }
+
+    public function peekNext() : Token
+    {
+        if (!$this->hasNext()) {
+            throw new \Exception('Unexpected end.');
+        }
+
+        return $this->tokens[$this->currentIndex + 1];
+    }
+
+    public function assertNext(string $tokenType) : Token
+    {
+        $token = $this->getNext();
+
+        if ($token->getType() !== $tokenType) {
+            throw new \Exception('Unexpected token.');
+        }
+
+        return $token;
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->tokens);
+    }
+}
