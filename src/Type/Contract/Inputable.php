fad4a8454da73e2a7a63021fcca60c3e2bb6974f
Václav Pelíšek <peldax@gmail.com>
2020-06-11 21:47:41 +0200

    Update Inputable.php

--- src/Type/Contract/Inputable.php
+++ src/Type/Contract/Inputable.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Graphpinator\Type\Contract;
 
-interface Inputable extends \Graphpinator\Type\Contract\Instantiable
+interface Inputable extends \Graphpinator\Type\Contract\Definition
 {
     public function applyDefaults($value);
 }
