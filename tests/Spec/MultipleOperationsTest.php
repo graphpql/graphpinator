38ec4c16a13f125699acfb2e08436ce6882dc1ba
Vossik <vosik.m@seznam.cz>
2020-08-29 19:06:17 +0200

    Improved MultiOperationsTest

--- tests/Spec/MultipleOperationsTest.php
+++ tests/Spec/MultipleOperationsTest.php
@@ -6,40 +6,35 @@ namespace Graphpinator\Tests\Spec;
 
 final class MultipleOperationsTest extends \PHPUnit\Framework\TestCase
 {
-    public function testOperationName() : void
+    public function requestDataProvider() : array
     {
-        $request = '{"query":"query queryName { field0 { field1(arg2: null) { name } } }","operationName":"queryName"}';
-
-        $graphpinator = new \Graphpinator\Graphpinator(\Graphpinator\Tests\Spec\TestSchema::getSchema());
-        $result = $graphpinator->runQuery(\Infinityloop\Utils\Json::fromString($request));
-
-        $expected = '{"field0":{"field1":{"name":"Test 123"}}}';
-
-        self::assertSame($expected, \Infinityloop\Utils\Json::fromArray($result->getData())->toString());
+        return [
+            [
+                '{"query":"query queryName { field0 { field1(arg2: null) { name } } }","operationName":"queryName"}',
+                '{"field0":{"field1":{"name":"Test 123"}}}',
+            ],
+            [
+                '{"query": "query queryName { field0 { field1 { name } } } query secondQueryName { aliasName: field0 { field1 { name } } }",' .
+                '"operationName": "queryName"}',
+                '{"field0":{"field1":{"name":"Test 123"}}}',
+            ],
+            [
+                '{"query": "query queryName { field0 { field1 { name } } } query secondQueryName { aliasName: field0 { field1 { name } } }",' .
+                '"operationName": "secondQueryName"}',
+                '{"aliasName":{"field1":{"name":"Test 123"}}}',
+            ],
+        ];
     }
 
-    public function testMultipleOperationsWithOperationName() : void
+    /**
+     * @dataProvider requestDataProvider
+     * @param string $request
+     * @param string $expected
+     */
+    public function testOperationName(string $request, string $expected) : void
     {
-        $request = '{"query": "query queryName { field0 { field1 { name } } } query secondQueryName { aliasName: field0 { field1 { name } } }"' .
-            ',"operationName": "queryName"}';
-
         $graphpinator = new \Graphpinator\Graphpinator(\Graphpinator\Tests\Spec\TestSchema::getSchema());
         $result = $graphpinator->runQuery(\Infinityloop\Utils\Json::fromString($request));
-
-        $expected = '{"field0":{"field1":{"name":"Test 123"}}}';
-
-        self::assertSame($expected, \Infinityloop\Utils\Json::fromArray($result->getData())->toString());
-    }
-
-    public function testMultipleOperationsWithOperationName2() : void
-    {
-        $request = '{"query": "query queryName { field0 { field1 { name } } } query secondQueryName { aliasName: field0 { field1 { name } } }",' .
-            '"operationName": "secondQueryName"}';
-
-        $graphpinator = new \Graphpinator\Graphpinator(\Graphpinator\Tests\Spec\TestSchema::getSchema());
-        $result = $graphpinator->runQuery(\Infinityloop\Utils\Json::fromString($request));
-
-        $expected = '{"aliasName":{"field1":{"name":"Test 123"}}}';
 
         self::assertSame($expected, \Infinityloop\Utils\Json::fromArray($result->getData())->toString());
     }
