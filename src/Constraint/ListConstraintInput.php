186c17c59bf28cb744151997284b5f95ac856413
Václav Pelíšek <peldax@gmail.com>
2020-09-21 13:44:45 +0200

    codestyle

--- src/Constraint/ListConstraintInput.php
+++ src/Constraint/ListConstraintInput.php
@@ -8,7 +8,7 @@ final class ListConstraintInput extends \Graphpinator\Type\InputType
 {
     protected const NAME = 'ListConstraintInput';
 
-    protected function getFieldDefinition(): \Graphpinator\Argument\ArgumentSet
+    protected function getFieldDefinition() : \Graphpinator\Argument\ArgumentSet
     {
         return new \Graphpinator\Argument\ArgumentSet([
             new \Graphpinator\Argument\Argument('minItems', \Graphpinator\Type\Container\Container::Int()),
