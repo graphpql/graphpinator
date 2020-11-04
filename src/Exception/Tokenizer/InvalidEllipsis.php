d2dc919329e1b47d0dbaf583899f9206eaf0649d
Václav Pelíšek <peldax@gmail.com>
2020-11-04 18:55:28 +0100

    made some error messages more clear

--- src/Exception/Tokenizer/InvalidEllipsis.php
+++ src/Exception/Tokenizer/InvalidEllipsis.php
@@ -6,5 +6,5 @@ namespace Graphpinator\Exception\Tokenizer;
 
 final class InvalidEllipsis extends \Graphpinator\Exception\Tokenizer\TokenizerError
 {
-    public const MESSAGE = 'Invalid ellipsis - three dots are expected for form ellipsis.';
+    public const MESSAGE = 'Invalid ellipsis - three dots are expected for ellipsis.';
 }
--- src/Exception/Tokenizer/SourceUnexpectedEnd.php
+++ src/Exception/Tokenizer/SourceUnexpectedEnd.php
@@ -6,5 +6,5 @@ namespace Graphpinator\Exception\Tokenizer;
 
 final class SourceUnexpectedEnd extends \Graphpinator\Exception\Tokenizer\TokenizerError
 {
-    public const MESSAGE = 'Unexpected end of input.';
+    public const MESSAGE = 'Unexpected end of input. Probably missing closing brace?';
 }
