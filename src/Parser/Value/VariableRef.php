b492ffc100ee7645fd6adcf257aef477fb16323d
Václav Pelíšek <peldax@gmail.com>
2020-11-20 23:08:42 +0100

    selected correct value class in variableRef

--- src/Parser/Value/VariableRef.php
+++ src/Parser/Value/VariableRef.php
@@ -25,10 +25,18 @@ final class VariableRef implements \Graphpinator\Parser\Value\Value
         return $this->varName;
     }
 
-    public function applyVariables(\Graphpinator\Resolver\VariableValueSet $variables) : Value
+    public function applyVariables(\Graphpinator\Resolver\VariableValueSet $variables) : \Graphpinator\Parser\Value\Value
     {
         if ($variables->offsetExists($this->varName)) {
             $value = $variables[$this->varName];
+
+            if ($value instanceof \Graphpinator\Value\ListValue) {
+                return new ListVal($value->getRawValue());
+            }
+
+            if ($value instanceof \Graphpinator\Value\InputValue) {
+                return new ObjectVal($value->getRawValue());
+            }
 
             return new \Graphpinator\Parser\Value\Literal($value->getRawValue());
         }
