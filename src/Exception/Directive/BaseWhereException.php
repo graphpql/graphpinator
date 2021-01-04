fff6125d80fbd7665c8d6ca8171e1be8f7b19ec1
Václav Pelíšek <peldax@gmail.com>
2021-01-04 13:02:01 +0100

    WhereExceptions outputable

--- src/Exception/Directive/BaseWhereException.php
+++ src/Exception/Directive/BaseWhereException.php
@@ -6,6 +6,11 @@ namespace Graphpinator\Exception\Directive;
 
 abstract class BaseWhereException extends \Graphpinator\Exception\GraphpinatorBase
 {
+    protected function isOutputable() : bool
+    {
+        return true;
+    }
+    
     protected function printType(\Graphpinator\Value\ResolvedValue $resolvedValue) : string
     {
         if ($resolvedValue->getRawValue() === null) {
