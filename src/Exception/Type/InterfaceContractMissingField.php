8f68450cb72e58c17825dee4cdec4d595a75c565
Václav Pelíšek <peldax@gmail.com>
2021-01-09 18:55:40 +0100

    Improved interface contract error message

--- src/Exception/Type/InterfaceContractMissingField.php
+++ src/Exception/Type/InterfaceContractMissingField.php
@@ -6,5 +6,12 @@ namespace Graphpinator\Exception\Type;
 
 final class InterfaceContractMissingField extends \Graphpinator\Exception\Type\TypeError
 {
-    public const MESSAGE = 'Type doesnt satisfy interface - missing field';
+    public const MESSAGE = 'Type "%s" doesnt satisfy interface "%s" - missing field "%s".';
+
+    public function __construct(string $childName, string $interfaceName, string $fieldName)
+    {
+        $this->messageArgs = [$childName, $interfaceName, $fieldName];
+
+        parent::__construct();
+    }
 }
