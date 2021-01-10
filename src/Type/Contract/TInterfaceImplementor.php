ed88383cf59990c49c3706e7b741799c190bcd23
Václav Pelíšek <peldax@gmail.com>
2021-01-10 16:55:15 +0100

    Improved condition

--- src/Type/Contract/TInterfaceImplementor.php
+++ src/Type/Contract/TInterfaceImplementor.php
@@ -86,8 +86,7 @@ trait TInterfaceImplementor
 
             if ($field->getArguments()->count() > $fieldContract->getArguments()->count()) {
                 foreach ($field->getArguments() as $argument) {
-                    if (!$fieldContract->getArguments()->offsetExists($argument->getName())
-                        && !$argument->getDefaultValue()) {
+                    if (!$fieldContract->getArguments()->offsetExists($argument->getName()) && $argument->getDefaultValue() === null) {
                         throw new \Graphpinator\Exception\Type\InterfaceContractNewArgumentWithoutDefault(
                             $this->getName(),
                             $interface->getName(),
