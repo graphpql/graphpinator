6567046f3be0c4b84ff09a344c93353eeff6aa00
Václav Pelíšek <peldax@gmail.com>
2021-01-10 17:04:51 +0100

    fixed misplaced condition

--- src/Type/Contract/TInterfaceImplementor.php
+++ src/Type/Contract/TInterfaceImplementor.php
@@ -82,17 +82,17 @@ trait TInterfaceImplementor
                         throw new \Graphpinator\Exception\Type\ArgumentConstraintNotContravariant();
                     }
                 }
-            }
-
-            if ($field->getArguments()->count() > $fieldContract->getArguments()->count()) {
-                foreach ($field->getArguments() as $argument) {
-                    if (!$fieldContract->getArguments()->offsetExists($argument->getName()) && $argument->getDefaultValue() === null) {
-                        throw new \Graphpinator\Exception\Type\InterfaceContractNewArgumentWithoutDefault(
-                            $this->getName(),
-                            $interface->getName(),
-                            $field->getName(),
-                            $argument->getName(),
-                        );
+                
+                if ($field->getArguments()->count() !== $fieldContract->getArguments()->count()) {
+                    foreach ($field->getArguments() as $argument) {
+                        if (!$fieldContract->getArguments()->offsetExists($argument->getName()) && $argument->getDefaultValue() === null) {
+                            throw new \Graphpinator\Exception\Type\InterfaceContractNewArgumentWithoutDefault(
+                                $this->getName(),
+                                $interface->getName(),
+                                $field->getName(),
+                                $argument->getName(),
+                            );
+                        }
                     }
                 }
             }
