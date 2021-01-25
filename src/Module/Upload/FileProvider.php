59b07dcbada363674377afe4fabd2a3479967413
Václav Pelíšek <peldax@gmail.com>
2021-01-25 20:05:31 +0100

    Made getMap return type nullable

--- src/Module/Upload/FileProvider.php
+++ src/Module/Upload/FileProvider.php
@@ -6,7 +6,7 @@ namespace Graphpinator\Module\Upload;
 
 interface FileProvider
 {
-    public function getMap() : \Infinityloop\Utils\Json\MapJson;
+    public function getMap() : ?\Infinityloop\Utils\Json\MapJson;
 
     public function getFile(string $key) : \Psr\Http\Message\UploadedFileInterface;
 }
