0d929f153d13c75ff31c1b551f38c5617e506a14
Václav Pelíšek <peldax@gmail.com>
2021-02-09 22:27:11 +0100

    fixed missing inheritance in directive definitions

--- src/Directive/Contract/ExecutableDefinition.php
+++ src/Directive/Contract/ExecutableDefinition.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Graphpinator\Directive\Contract;
 
-interface ExecutableDefinition extends Definition
+interface ExecutableDefinition extends Definition, TypeDependantDefinition
 {
     public function resolveFieldBefore(
         \Graphpinator\Value\ArgumentValueSet $arguments,
