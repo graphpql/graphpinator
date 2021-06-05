b6ace1704fe850643c474a4406452a2989c08310
Václav Pelíšek <peldax@gmail.com>
2021-06-05 19:43:46 +0200

    Added some comments to interfaces

--- src/Typesystem/Component.php
+++ src/Typesystem/Component.php
@@ -4,6 +4,13 @@ declare(strict_types = 1);
 
 namespace Graphpinator\Typesystem;
 
+/**
+ * Interface Component
+ *
+ * Any component of typesystem.
+ * For example some more important entities like Type or Directive definition,
+ * but also less important like field, argument, enum item or directive usage.
+ */
 interface Component
 {
     public function accept(\Graphpinator\Typesystem\ComponentVisitor $visitor) : mixed;
--- src/Typesystem/Entity.php
+++ src/Typesystem/Entity.php
@@ -4,6 +4,11 @@ declare(strict_types = 1);
 
 namespace Graphpinator\Typesystem;
 
+/**
+ * Interface Entity
+ *
+ * Entity is a first class citizen in typesystem, currently Schema, Type definition and Directive definition.
+ */
 interface Entity extends \Graphpinator\Typesystem\Component
 {
     public function accept(\Graphpinator\Typesystem\EntityVisitor $visitor) : mixed;
