e51fa0452abd557948201cb8d6908a76e12744f4
Václav Pelíšek <peldax@gmail.com>
2023-03-31 15:56:12 +0200

    Refactoring and codestyle of Module namespace

--- src/Module/Module.php
+++ src/Module/Module.php
@@ -4,31 +4,19 @@ declare(strict_types = 1);
 
 namespace Graphpinator\Module;
 
+use Graphpinator\Parser\ParsedRequest;
+use \Graphpinator\Normalizer\NormalizedRequest;
+use \Graphpinator\Normalizer\FinalizedRequest;
+
 interface Module
 {
-    public function processRequest(
-        \Graphpinator\Request\Request $request,
-    ) : \Graphpinator\Request\Request|
-        \Graphpinator\Parser\ParsedRequest|
-        \Graphpinator\Normalizer\NormalizedRequest|
-        \Graphpinator\Normalizer\FinalizedRequest;
+    public function processRequest(\Graphpinator\Request\Request $request) : \Graphpinator\Request\Request|ParsedRequest|NormalizedRequest|FinalizedRequest;
 
-    public function processParsed(
-        \Graphpinator\Parser\ParsedRequest $request,
-    ) : \Graphpinator\Parser\ParsedRequest|
-        \Graphpinator\Normalizer\NormalizedRequest|
-        \Graphpinator\Normalizer\FinalizedRequest;
+    public function processParsed(ParsedRequest $request) : ParsedRequest|NormalizedRequest|FinalizedRequest;
 
-    public function processNormalized(
-        \Graphpinator\Normalizer\NormalizedRequest $request,
-    ) : \Graphpinator\Normalizer\NormalizedRequest|
-        \Graphpinator\Normalizer\FinalizedRequest;
+    public function processNormalized(NormalizedRequest $request) : NormalizedRequest|FinalizedRequest;
 
-    public function processFinalized(
-        \Graphpinator\Normalizer\FinalizedRequest $request,
-    ) : \Graphpinator\Normalizer\FinalizedRequest;
+    public function processFinalized(FinalizedRequest $request) : FinalizedRequest;
 
-    public function processResult(
-        \Graphpinator\Result $result,
-    ) : \Graphpinator\Result;
+    public function processResult(\Graphpinator\Result $result) : \Graphpinator\Result;
 }
