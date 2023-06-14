a2df4d85f0d010be37a550b4d7559d9041c92eeb
Václav Pelíšek <peldax@gmail.com>
2023-06-14 07:20:43 +0200

    Schema is not final

--- src/Typesystem/Schema.php
+++ src/Typesystem/Schema.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Graphpinator\Typesystem;
 
-final class Schema implements \Graphpinator\Typesystem\Contract\Entity
+class Schema implements \Graphpinator\Typesystem\Contract\Entity
 {
     use \Nette\SmartObject;
     use \Graphpinator\Typesystem\Utils\TOptionalDescription;
@@ -44,32 +44,32 @@ final class Schema implements \Graphpinator\Typesystem\Contract\Entity
         ])));
     }
 
-    public function getContainer() : Container
+    final public function getContainer() : Container
     {
         return $this->container;
     }
 
-    public function getQuery() : Type
+    final public function getQuery() : Type
     {
         return $this->query;
     }
 
-    public function getMutation() : ?Type
+   final  public function getMutation() : ?Type
     {
         return $this->mutation;
     }
 
-    public function getSubscription() : ?Type
+    final public function getSubscription() : ?Type
     {
         return $this->subscription;
     }
 
-    public function accept(\Graphpinator\Typesystem\Contract\EntityVisitor $visitor) : mixed
+    final public function accept(\Graphpinator\Typesystem\Contract\EntityVisitor $visitor) : mixed
     {
         return $visitor->visitSchema($this);
     }
 
-    public function addDirective(
+    final public function addDirective(
         \Graphpinator\Typesystem\Location\SchemaLocation $directive,
         array $arguments = [],
     ) : self
