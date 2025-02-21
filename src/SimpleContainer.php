84919d945f56e824443ce0e84bb9f6a43e5fa575
Patrik <patrik.safranek@webthinx.com>
2025-02-21 08:36:26 +0100

    removed testing dumps

--- src/SimpleContainer.php
+++ src/SimpleContainer.php
@@ -64,11 +64,9 @@ class SimpleContainer extends Container
         $typeCount = 0;
 
         foreach ($types as $type) {
-            var_dump($type->getName());
             $this->types[$type->getName()] = $type;
             $typeCount++;
         }
-//        die();
 
         $directivesCount = 0;
 
@@ -80,8 +78,6 @@ class SimpleContainer extends Container
         if (Graphpinator::$validateSchema &&
             ($typeCount !== \count($this->types) ||
             $directivesCount !== \count($this->directives))) {
-            var_dump($types, $this->types,$directives, $this->directives);
-//            die();
             throw new TypeNamesNotUnique();
         }
 
